9065bedfe8b4ce8321c75b5f2a6abea52de09931
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2023-10-25 10:47:29 -0300

    (att): fix no código de validação cnpj

--- 01 - Validadores/validar_cnpj.js
+++ 01 - Validadores/validar_cnpj.js
@@ -31,20 +31,20 @@ function validateCNPJ(cnpj) {
         return false
     }
 
-    if (cnpj.length != 14) {
+    if (cnpj.length !== 14) {
         return false
     }
 
-    if (cnpj == "00000000000000" ||
-        cnpj == "11111111111111" ||
-        cnpj == "22222222222222" ||
-        cnpj == "33333333333333" ||
-        cnpj == "44444444444444" ||
-        cnpj == "55555555555555" ||
-        cnpj == "66666666666666" ||
-        cnpj == "77777777777777" ||
-        cnpj == "88888888888888" ||
-        cnpj == "99999999999999") {
+    if (cnpj === "00000000000000" ||
+        cnpj === "11111111111111" ||
+        cnpj === "22222222222222" ||
+        cnpj === "33333333333333" ||
+        cnpj === "44444444444444" ||
+        cnpj === "55555555555555" ||
+        cnpj === "66666666666666" ||
+        cnpj === "77777777777777" ||
+        cnpj === "88888888888888" ||
+        cnpj === "99999999999999") {
         return false;
     }
 
@@ -59,7 +59,7 @@ function validateCNPJ(cnpj) {
         pos = 9;
     }
     var result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result != digits.charAt(0)) {
+    if (result !== digits.charAt(0)) {
         return false;
     }
     length = length + 1;
@@ -73,7 +73,7 @@ function validateCNPJ(cnpj) {
         }
     }
     result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result != digits.charAt(1)) {
+    if (result !== digits.charAt(1)) {
         return false;
     }
 
